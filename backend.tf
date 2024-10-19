faabd844f73558d993b38a7f5c2b7ab5aa896ba1
EC2 Default User <ec2-user@ip-172-31-44-38.ec2.internal>
2024-10-19 18:01:45 +0000

    Update README

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cls12-network-gleisonfreire"
+    bucket = "clc12-network-gleisonfreire"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
   }
